f24be314773633af831a9eb9a224094c2e6b38be
play93 <95739833+play93@users.noreply.github.com>
2024-09-25 23:42:09 +0900

    Project Start

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header></header>
         {children}
       </body>
     </html>
